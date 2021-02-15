035e3a8a9d6d47b730f44b0c241300f5b4355564
Tsuneki Umeda <umedat@amazon.co.jp>
2021-02-15 17:18:52 +0900

    generalize aws signer

--- lib/awsSigner.ts
+++ lib/awsSigner.ts
@@ -1,7 +1,7 @@
 const core = require('aws-sdk/lib/core')
 const aws = require('aws-sdk')
 const { Auth } = require('./Auth')
-import { accessKey, secretKey } from '../credentials'
+import { accessKey, secretKey, endpoint, region } from '../credentials'
 import { ApiOptions } from './DefaultApiClient'
 import * as qs from 'qs'
 const credential = new aws.Credentials(accessKey, secretKey)
@@ -16,12 +16,12 @@ export class awsSigner {
 
     const options = {
       // hostname: 'sellingpartnerapi-na.amazon.com',
-      region: 'us-east-1',
+      region: region,
       method: this.apiOptions.method,
       search: () => (queryString ? queryString : ''),
       body,
       headers: {
-        host: 'sellingpartnerapi-na.amazon.com',
+        host: endpoint,
         'x-amz-access-token': accessToken,
       },
       pathname: () => this.apiOptions.pathname,
@@ -34,7 +34,6 @@ export class awsSigner {
     const now = new Date()
     // // V4クラスのインスタンスを作成
     const signer = new core.Signers.V4(options, serviceName)
-
     // // SigV4署名
     signer.addAuthorization(credential, now)
     return options
